97679d453b3b4696867ad080ea56204493b95604
aymanehassari <hassari.aymane@iisgalvanimi.edu.it>
2021-02-08 08:28:17 +0000

    punto 2

--- email-hassari/src/app/app.component.ts
+++ email-hassari/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'email-hassari';
+  addEmail(destinatario: HTMLInputElement, oggetto: HTMLInputElement, testo: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${destinatario.value} and link: ${oggetto.value} and testo: ${testo.value}`);
+  return false;
+  }
 }
+
